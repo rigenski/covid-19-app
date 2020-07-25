1c54a1c55c43cf6c1fbc9eb61cd0e60cfeb15c81
rygenzx <maulana.zx525@gmail.com>
2020-07-26 01:51:35 +0700

    add file corona app

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,47 @@
-const searchCountry = () => {
-    const input = document.getElementById('input-search')
-    const country = document.getElementById('title-country');
-    const endPoint = 'https://api.covid19api.com/summary';
+// const searchCountry = () => {
+//     let inputKeyword = document.getElementById('input-keyword')
+//     const regency = document.getElementById('title-regency');
+//     const endPoint = 'https://api.covid19api.com/summary';
 
-    input.addEventListener("keyup", async () => {
-        try {
-            const req = await fetch(endPoint)
-            const res = await req.json();
-            res.Search.forEach(covid => {
+//     inputKeyword.addEventListener("keyup", async () => {
+//         try {
+//             const req = await fetch(endPoint)
+//             const res = await req.json();
+//             res.Search.forEach(covid => {
                 
-                country.innerHTML = 
-                `<h1 class="text-center">${covid.Countrie[0].Country}</h1>`;
-            })
-        } catch(error) {
-            console.log(error);
-        }
-    })
-};
+//                 for(let i in covid.list_data){
+//                     console.log(`${covid.list_data[i].key}`);
+//                     }
+//             })
+//         } catch(error) {
+//             console.log(error);
+//         }
+//     })
+// }; 
 
-searchCountry()
+const reqApi = async () => {
+    const endPoint = 'https://api.covid19api.com/summary';
+    let positif = document.getElementById('positif');
+    let newPositif = document.getElementById('newPositif');
+    let sembuh = document.getElementById('sembuh');
+    let newSembuh = document.getElementById('newSembuh')
+    let meninggal = document.getElementById('meninggal');
+    let newMeninggal = document.getElementById('newMeninggal')
+
+    try{
+        const req = await fetch(endPoint);
+        const res = await req.json();
+
+        positif.innerHTML = res.Global.TotalConfirmed;
+        newPositif.innerHTML = `<p class="card-text">+${res.Global.NewConfirmed}</p>`
+        sembuh.innerHTML = res.Global.TotalRecovered;
+        newSembuh.innerHTML = `<p class="card-text">+${res.Global.NewRecovered}</p>`
+        meninggal.innerHTML = res.Global.TotalDeaths;
+        newMeninggal.innerHTML = `<p class="card-text">+${res.Global.NewDeaths}</p>`
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+reqApi()
